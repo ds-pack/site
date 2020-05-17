73ae9177c13d2b0038b0eadb21c0a24dc24e07ab
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-05-17 17:16:42 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Box, Text } from '@matthamlin/component-library'
+
+export default function App() {
+  return (
+    <Box>
+      <H1>Hello, World!</H1>
+    </Box>
+  )
+}
